f529a910eab79d2c1ff0d09188d80769cae68367
Vinicius <vini_carval@hotmail.com>
2017-05-17 02:32:18 -0300

    UsuarioWS adicionado e main atualizada

--- SOSReal/app/src/main/java/pap/com/sosreal/MainActivity.java
+++ SOSReal/app/src/main/java/pap/com/sosreal/MainActivity.java
@@ -1,81 +1,80 @@
 package pap.com.sosreal;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
-    Usuario usu = new Usuario();
+    private Usuario usu = new Usuario();
+    private List<Usuario> lista = new ArrayList<Usuario>();
+    private UsuarioService service = new UsuarioService();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void entrar(View view){
+    public void entrar(View view) {
         EditText usuario = (EditText) findViewById(R.id.txtUsuario);
         EditText senha = (EditText) findViewById(R.id.txtSenha);
         TextView text = (TextView) findViewById(R.id.lblErro);
 
-            if (verificaUsuario() == true) {
-                Bundle dados = new Bundle();
-                dados.putString("usuario",usu.getUsuario());
-                dados.putString("email",usu.getEmail());
-                dados.putInt("id",usu.getId());
+        if (verificaUsuario() == true) {
+            new CarregarUsuario().execute();
 
-                Intent i = new Intent(this, PrincipalActivity.class);
-                i.putExtras(dados);
-                startActivity(i);
-                finish();
+        } else if (senha.getText().toString().equals("") || usuario.getText().toString().equals("")) {
+            text.setText(R.string.erroVazio);
+        } else {
 
-            }else if(senha.getText().toString().equals("") || usuario.getText().toString().equals("")){
-                text.setText(R.string.erroVazio);
-            }
-            else {
-
-                text.setText(R.string.erroLogin);
-            }
+            text.setText(R.string.erroLogin);
+        }
 
 
     }
 
-    public boolean verificaUsuario(){
+    public boolean verificaUsuario() {
         EditText usuario = (EditText) findViewById(R.id.txtUsuario);
         EditText senha = (EditText) findViewById(R.id.txtSenha);
 
 
         try {
-            usu = new UsuarioDB().buscarPorUsuESenha(usuario.getText().toString(), senha.getText().toString());
+            usu = new UsuarioService().buscarPorUsuESenha(usuario.getText().toString(), senha.getText().toString());
             return true;
-        }
-        catch(IllegalArgumentException e){
+
+        } catch (IllegalArgumentException e) {
             return false;
         }
     }
 
-    public void criarConta(View view){
+    public void criarConta(View view) {
 
-        Intent i = new Intent(this,CriarUsuarioActivity.class);
-        startActivityForResult(i,1);
+        Intent i = new Intent(this, CriarUsuarioActivity.class);
+        startActivityForResult(i, 1);
     }
 
-    public void alterarSenha(View view){
-        Intent i = new Intent(this,SenhaAleatoriaActivity.class);
-        startActivityForResult(i,2);
+    public void alterarSenha(View view) {
+        Intent i = new Intent(this, SenhaAleatoriaActivity.class);
+        startActivityForResult(i, 2);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         if (requestCode == 1) {
-            if(resultCode == Activity.RESULT_OK){
+            if (resultCode == Activity.RESULT_OK) {
                 Bundle dados = data.getExtras();
 
                 EditText usuario = (EditText) findViewById(R.id.txtUsuario);
@@ -87,8 +86,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public static boolean validar(String email)
-    {
+    public static boolean validar(String email) {
         boolean isEmailIdValid = false;
         if (email != null && email.length() > 0) {
             String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
@@ -99,5 +97,45 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return isEmailIdValid;
+    }
+
+    /*@Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (dialog != null) {
+            dialog.dismiss();
+            dialog = null;
+        }
+    }*/
+
+    private class CarregarUsuario extends AsyncTask<String, Void, Usuario> {
+        private ProgressDialog dialog;
+
+        @Override
+        protected void onPreExecute() {
+            dialog = new ProgressDialog(MainActivity.this);
+            dialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(Usuario usuario) {
+            usu = usuario;
+            Bundle dados = new Bundle();
+            dados.putString("usuario", usu.getUsuario());
+            dados.putString("email", usu.getEmail());
+            dados.putInt("id", usu.getId());
+
+            Intent i = new Intent(MainActivity.this, PrincipalActivity.class);
+            i.putExtras(dados);
+            startActivity(i);
+            dialog.dismiss();
+            dialog = null;
+            finish();
+        }
+
+        @Override
+        protected Usuario doInBackground(String... params) {
+            return service.getById(1);
+        }
     }
 }
---
+++ SOSReal/app/src/main/java/pap/com/sosreal/UsuarioService.java
@@ -0,0 +1,138 @@
+package pap.com.sosreal;
+
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class UsuarioService {
+    Usuario usu = new Usuario();
+
+    /*public Usuario buscarUsuario(int id) {
+        usu.setId(1);
+        usu.setEmail("email@email.com");
+        usu.setSenha("loco");
+        usu.setUsuario("loco");
+
+        return usu;
+    }
+    */
+    public Usuario buscarPorUsuESenha(String usuario, String senha) throws IllegalArgumentException {
+        usu.setId(1);
+        usu.setEmail("email@email.com");
+        usu.setSenha("loco");
+        usu.setUsuario("loco");
+
+        if (usuario.equals("loco") && senha.equals("loco")) {
+            return usu;
+        } else {
+            throw new IllegalArgumentException("Usuário não encontrado");
+        }
+    }
+
+    private static final String URL = "http://pap5.ga/ws/usuario";
+
+    public List<Usuario> getAll() {
+        List<Usuario> usuarios = new ArrayList<>();
+        HttpURLConnection urlConnection = null;
+
+        try {
+            URL url = new URL(URL);
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            Scanner s = new Scanner(in);
+            String conteudo = s.useDelimiter("\\A").next();
+
+            Gson gson = new Gson();
+            usuarios = gson.fromJson(conteudo, new TypeToken<List<Usuario>>() {
+            }.getType());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            urlConnection.disconnect();
+        }
+        return usuarios;
+    }
+
+    public Usuario getById(int id) {
+        Usuario usu = new Usuario();
+        HttpURLConnection urlConnection = null;
+
+        try {
+            URL url = new URL(URL + "/" + id);
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            Scanner s = new Scanner(in);
+            String conteudo = s.useDelimiter("\\A").next();
+
+            Gson gson = new Gson();
+            usu = gson.fromJson(conteudo, Usuario.class);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            urlConnection.disconnect();
+        }
+        return usu;
+    }
+
+    public void post(Usuario usuario) {
+        HttpURLConnection urlConnection = null;
+
+        try {
+            URL url = new URL(URL);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setDoOutput(true);
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+            urlConnection.setRequestProperty("Accept", "application/json");
+
+            OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
+            Writer w = new BufferedWriter(new OutputStreamWriter(out));
+
+            Gson gson = new Gson();
+            String json = gson.toJson(usuario);
+
+            w.write(json);
+            w.close();
+
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            Scanner s = new Scanner(in);
+            String conteudo = s.nextLine();
+            in.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+    public void delete(String id) {
+        HttpURLConnection urlConnection = null;
+
+        try {
+            URL url = new URL(URL + "/" + id);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("DELETE");
+
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
+
